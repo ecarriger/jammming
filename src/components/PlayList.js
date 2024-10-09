54407f7dfb419207c082ab213d8b851e68f1e3d8
Ethan Carriger <ethancarriger@gmail.com>
2024-10-09 14:20:05 -0400

    Add playlist name field

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -5,10 +5,12 @@ const PlayList = () => {
     return (
         <div>
             <h1>Playlist</h1>
-            <TrackList />
             <form>
+                <label for="playlist-name">Playlist name</label>
+                <input type="text" id="playlist-name" name="playlist-name" placeholder="Playlist name" />
                 <input type='submit' value='Save to Spotify' />
             </form>
+            <TrackList />
         </div>
     );
 };
