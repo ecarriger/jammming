43bef7248451aa25bb9dab26337149fdd6303491
Ethan Carriger <ethancarriger@gmail.com>
2024-10-11 10:17:58 -0400

    Set up playlist name state and bound to input

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TrackList from './TrackList';
 
 const PlayList = ({tracks, handleTrackClick}) => {
+
+    const [playlistName, setPlaylistName] = useState('');
+    const handlePlaylistNameChange = ({target}) => {
+        setPlaylistName(target.value);
+    };
+
     return (
         <section>
             <h2>Playlist</h2>
             <form>
                 <label htmlFor="playlist-name">Playlist name</label>
-                <input type="text" id="playlist-name" name="playlist-name" placeholder="Playlist name" />
+                <input 
+                    type="text" 
+                    id="playlist-name" 
+                    name="playlist-name" 
+                    placeholder="Playlist name" 
+                    value={playlistName}
+                    onChange={handlePlaylistNameChange} 
+                />
                 <input type='submit' value='Save to Spotify' />
             </form>
             <TrackList tracks={tracks} handleTrackClick={handleTrackClick} />
