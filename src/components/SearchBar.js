b9233d2e012b64c67e621738621bf8d716660eb6
Ethan Carriger <ethancarriger@gmail.com>
2024-10-14 16:44:18 -0400

    Set for to test mock API key

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const SearchBar = () => {
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        console.log(process.env.REACT_APP_SPOTIFY_API_KEY);
+    };
+
     return (
         <section>
             <h2>Search</h2>
-            <form>
+            <form onSubmit={handleSearchSubmit} >
                 <label htmlFor="search-bar">Search for songs</label>
                 <input type="text" id="search-bar" name="search-bar" placeholder="Type your search..." />
                 <input type="submit" value="Search" />
