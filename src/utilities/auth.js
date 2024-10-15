5c98c7f29cdbec4c9b1df34a210060923dec9aa0
Ethan Carriger <ethancarriger@gmail.com>
2024-10-15 11:34:13 -0400

    Add auth template from Spotify docs

--- src/utilities/auth.js
+++ src/utilities/auth.js
@@ -1,3 +1,18 @@
-const auth = () => {};
+const auth = () => {
+    var client_id = '7549faaac87744f98288992bdaadfbde';
+    var redirect_uri = 'http://localhost:3000/auth';
+
+    var state = generateRandomString(16);
+
+    localStorage.setItem(stateKey, state);
+    var scope = 'playlist-modify-private playlist-modify-public';
+
+    var url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&client_id=' + encodeURIComponent(client_id);
+    url += '&scope=' + encodeURIComponent(scope);
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    url += '&state=' + encodeURIComponent(state);
+};
 
 export default auth;
