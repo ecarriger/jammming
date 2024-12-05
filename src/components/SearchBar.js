773f00891b6d64976b42213b9276748d373041c8
Ethan Carriger <ethancarriger@gmail.com>
2024-12-05 14:47:56 -0500

    Add tests

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,50 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Spotify from '../utilities/Spotify';
 import { checkTokenExpired } from '../utilities/utils';
 
 
 const SearchBar = ({setResultTracks, setAuth, accessToken, accessTokenExpiration}) => {
-    const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    if(message) {
+      setTimeout(() => {
+        setMessage('');
+      }, 5000)
+    }
+  }, [message])
 
-    const handleSearchChange = ({target}) => {
-        setSearchQuery(target.value);
-    };
-
-    //Send search request to Spotify
-  const handleSearchSubmit = async (event) => {
-    event.preventDefault();
-
-  if(checkTokenExpired(accessTokenExpiration)) {
-    setAuth(false);
-    window.location = 'http://localhost:3000';
-    return;
-  }
-
-    const formData = new FormData(event.target);
-    const query = formData.get('search-bar');    
-
-    const spotifyResults = await Spotify.getTracks(query, accessToken);
-    setResultTracks(spotifyResults.tracks.items);
+  const handleSearchChange = ({target}) => {
+      setSearchQuery(target.value);
   };
 
-    return (
-        <section>
-            <h2>Search</h2>
-            <form onSubmit={handleSearchSubmit}>
-                <label htmlFor="search-bar">Search for songs</label>
-                <input 
-                    type="text"
-                    id="search-bar" 
-                    name="search-bar" 
-                    value={searchQuery} 
-                    onChange={handleSearchChange}
-                    placeholder="Type your search..." />
-                <input type="submit" value="Search" />
-            </form>
-        </section>
-        
-    );
+
+const queueRedirect = () => {
+  setTimeout(() => {
+    window.location = 'http://localhost:3000';
+  }, 3000)
+};
+//Send search request to Spotify
+const handleSearchSubmit = async (event) => {
+  event.preventDefault();
+
+if(SearchBar.length < 3) {
+  setMessage('Search must be at least 3 charaters');
+  return;
+}
+
+if(checkTokenExpired(accessTokenExpiration)) {
+  setAuth(false);
+  setMessage('Authentication expired, redirecting...');
+  queueRedirect();
+  return;
+}
+
+  const formData = new FormData(event.target);
+  const query = formData.get('search-bar');    
+
+  const spotifyResults = await Spotify.getTracks(query, accessToken);
+  setResultTracks(spotifyResults.tracks.items);
+};
+
+  return (
+      <section>
+          <h2>Search</h2>
+          <form onSubmit={handleSearchSubmit}>
+              <label htmlFor="search-bar">Search for songs</label>
+              <input 
+                  type="text"
+                  id="search-bar" 
+                  name="search-bar" 
+                  value={searchQuery} 
+                  onChange={handleSearchChange}
+                  placeholder="Type your search..." />
+              <input type="submit" value="Search" />
+          </form>
+          <p>{message}</p>
+      </section>
+      
+  );
 };
 
 export default SearchBar;
--- src/components/SearchBar.test.js
+++ src/components/SearchBar.test.js
@@ -1,10 +1,36 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, waitFor } from '@testing-library/react';
+import user from '@testing-library/user-event';
+
 import SearchBar from './SearchBar';
 
 const renderSearchBar = (setResultTracks = () => {}, setAuth = () => {}) => {
     render(<SearchBar setResultTracks={setResultTracks} setAuth={setAuth} />)
 };
 
-test('First test', () => {
+test('Searchbar saves value', async () => {
     renderSearchBar();
+
+    const searchbar = screen.getByRole('textbox', {
+        name: /search for songs/i
+    });
+    expect(searchbar).toBeInTheDocument();
+
+    user.click(searchbar);
+    user.keyboard('sound of silence');
+
+    expect(searchbar.value).toBe('sound of silence');
 });
+test('Message shown if search to short', async () => {
+    renderSearchBar();
+
+    const message = await screen.findByText(/search must be at least 3 charaters/i);
+
+    expect(message).toBeInTheDocument();
+});
+test('Message shown if token expired', async () => {
+    renderSearchBar();
+
+    const message = await screen.findByText(/authentication expired/i);
+
+    expect(message).toBeInTheDocument();
+})
