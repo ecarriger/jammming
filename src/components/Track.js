ead0d7ec402d95d3fec1c888a655d8738f58fa01
Ethan Carriger <ethancarriger@gmail.com>
2025-01-02 14:49:13 -0500

    add cover art to tracks and remove album name

--- src/components/Track.js
+++ src/components/Track.js
@@ -5,9 +5,9 @@ const Track = ({index, track, handleTrackClick}) => {
 
     return (
         <li key={index} onClick={handleTrackClick}>
+            <img src={track.album.images[2].url} alt='Cover art' />
             <h3>{track.name}</h3>
             <h4>{track.artists[0].name}</h4>
-            <h4>{track.album.name}</h4>
             <h4>{convertMsToTime(track.duration_ms)}</h4>
         </li>
     );
