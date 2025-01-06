5900734883ebe9f059c4c2111afa5832ddb3417c
Ethan Carriger <ethancarriger@gmail.com>
2025-01-06 15:13:48 -0500

    footer styles

--- src/App.js
+++ src/App.js
@@ -19,38 +19,41 @@ function App() {
 
   //App JSX to render
   return (
-    <div>
-      <header>
-        <img className='logo' src='/' alt='Jammming headphones logo' />
-        <h1 className='galada-regular' >Jammming</h1>
-        <p>Search for songs on Spotify and create a playlist</p>
-      </header>
-      <main>
-        {!auth && <Auth 
-          auth={auth} 
-          setAuth={setAuth} 
-          accessToken={accessToken}
-          setAccessToken={setAccessToken}
-          setAccessTokenExpiration={setAccessTokenExpiration}
-        />}
-        {auth && <SearchBar 
-          setResultTracks={setResultTracks} 
-          setAuth={setAuth} 
-          accessToken={accessToken}
-          accessTokenExpiration={accessTokenExpiration}
-        />}
-        {auth && <SearchResults resultTracks={resultTracks} playlistTracks={playlistTracks} setPlaylistTracks={setPlaylistTracks} />}
-        {auth && <Playlist 
-          playlistTracks={playlistTracks} 
-          setPlaylistTracks={setPlaylistTracks} 
-          auth={auth}
-          setAuth={setAuth}
-          accessToken={accessToken}
-          accessTokenExpiration={accessTokenExpiration}
-        />}
-      </main>
+    <div id="app">
+      <div id="upper-content">
+        <header>
+          <img className='logo' src='/' alt='Jammming headphones logo' />
+          <h1 className='galada-regular' >Jammming</h1>
+          <p>Search for songs on Spotify and create a playlist</p>
+        </header>
+        <main>
+          {!auth && <Auth 
+            auth={auth} 
+            setAuth={setAuth} 
+            accessToken={accessToken}
+            setAccessToken={setAccessToken}
+            setAccessTokenExpiration={setAccessTokenExpiration}
+          />}
+          {auth && <SearchBar 
+            setResultTracks={setResultTracks} 
+            setAuth={setAuth} 
+            accessToken={accessToken}
+            accessTokenExpiration={accessTokenExpiration}
+          />}
+          {auth && <SearchResults resultTracks={resultTracks} playlistTracks={playlistTracks} setPlaylistTracks={setPlaylistTracks} />}
+          {auth && <Playlist 
+            playlistTracks={playlistTracks} 
+            setPlaylistTracks={setPlaylistTracks} 
+            auth={auth}
+            setAuth={setAuth}
+            accessToken={accessToken}
+            accessTokenExpiration={accessTokenExpiration}
+          />}
+        </main>
+      </div>
       <footer>
-
+          <p>Created by <a href="https://github.com/ecarriger/jammming">@ecarriger on GitHub</a></p>
+          <p><a href="/">Privacy Policy</a></p>
       </footer>
     </div>
   )
